edb073aef07ea5d993c9e253f7131efbfaf52ed2
thevisionarysintegrator <105822187+thevisionarysintegrator@users.noreply.github.com>
2022-05-18 16:46:21 -0500

    Create lily-chiller-tracking.js

---
+++ lily-chiller-tracking.js
@@ -0,0 +1,32 @@
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+var sourceparam = getUrlParameter('source_id');
+var source = Cookies.get('source_id');
+if (source == null || source == ""){
+	if (sourceparam != "") {
+		Cookies.set('source_id', sourceparam, { expires: 365 });}}
+	else{
+		Cookies.set('source_id', 'wo', { expires: 365 });}} //If no source URL Parameter present, then assume source was 'wo' or 'website organic'
+
+var targetparam = getUrlParameter('target_id');
+var target = Cookies.get('target_id');
+if (target == null || target == ""){
+	if (targetparam != "") {
+		Cookies.set('target_id', targetparam, { expires: 365 });}}
+ 
+var adparam = getUrlParameter('ad_id');
+var ad = Cookies.get('ad_id');
+if (ad == null || ad == ""){
+	if (adparam != "") {
+		Cookies.set('ad_id', adparam, { expires: 365 });}}
+
+var fbclidparam = getUrlParameter('fbclid');
+var fbclid = Cookies.get('fbclid');
+if (fbclid == null || fbclid == ""){
+	if (fbclidparam != "") {
+		Cookies.set('campaign_id', fbclidparam, { expires: 365 });}}
