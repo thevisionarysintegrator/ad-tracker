fbd0e69929c18f4d558fed28d52def54f902ac7a
thevisionarysintegrator <105822187+thevisionarysintegrator@users.noreply.github.com>
2022-06-06 16:39:59 -0500

    Add ajax call

--- lily-chiller-tracking.js
+++ lily-chiller-tracking.js
@@ -12,22 +12,35 @@ if (source == null || source == ""){
 		Cookies.set('source_id', sourceparam, { expires: 365 });}
 	else{
 		Cookies.set('source_id', 'wo', { expires: 365 });}} //If no source URL Parameter present, then assume source was 'wo' or 'website organic'
+else {
+	sourceparam = source;
+}
 
 var targetparam = getUrlParameter('target_id');
 var target = Cookies.get('target_id');
 if (target == null || target == ""){
 	if (targetparam != "") {
 		Cookies.set('target_id', targetparam, { expires: 365 });}}
+else {
+	targetparam = target;
+}
  
 var adparam = getUrlParameter('ad_id');
 var ad = Cookies.get('ad_id');
 if (ad == null || ad == ""){
 	if (adparam != "") {
 		Cookies.set('ad_id', adparam, { expires: 365 });}}
+else {
+	adparam = ad;
+}
 
 var fbclidparam = getUrlParameter('fbclid');
 var fbclid = Cookies.get('fbclid');
 if (fbclid == null || fbclid == ""){
 	if (fbclidparam != "") {
 		Cookies.set('campaign_id', fbclidparam, { expires: 365 });}}
- 
+else {
+	fbclidparam = fbclid;
+}
+		
+$.post( "https://hooks.zapier.com/hooks/catch/7659845/bah9a97/", { source_id: sourceparam, target_id: targetparam, ad_id: adparam, fbclid: fbclidparam, page: $(location).attr("href") } );
